87782e9fd57f9e8f226021833c1936c97af8df43
mwcox <mwcox@comcast.net>
2019-04-23 07:54:49 -0400

    Borrowed sparkfun PID & VID since we can't make our own drivers

--- boards/uconnfsae-dashboard/board_config.h
+++ boards/uconnfsae-dashboard/board_config.h
@@ -5,8 +5,8 @@
 #define PRODUCT_NAME "Dashboard-V2.0"
 #define VOLUME_LABEL "DASHBOOT"
 
-#define USB_VID 0x03EB   // Atmel
-#define USB_PID 0x2402   // Generic HID device
+#define USB_VID 0x1B4F   // Borrowing Sparkfun VID & PID
+#define USB_PID 0x0D23   // So it uses their USB drivers
 
 #define BOARD_ID "Dashboard-V2.0"
 
